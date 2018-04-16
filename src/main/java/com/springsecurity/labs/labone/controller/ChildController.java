db59a5a2f39d859dc7975afc05a89e6870d53738
MuhammedMahrous <muhamed.mahrous.muhamed.kamel@gmail.com>
2018-04-16 15:15:20 +0200

    completed logic.

---
+++ src/main/java/com/springsecurity/labs/labone/controller/ChildController.java
@@ -0,0 +1,71 @@
+package com.springsecurity.labs.labone.controller;
+
+import com.springsecurity.labs.labone.service.MyService;
+import com.springsecurity.labs.labone.util.SpringContext;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChildController {
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+    @FXML
+    private TextField role;
+
+    private SecurityContext securityContext;
+
+    public void doSomething(ActionEvent actionEvent) {
+        ApplicationContext applicationContext = SpringContext.getApplicationContext();
+        MyService myService = (MyService) applicationContext.getBean("myService");
+
+        String userName = username.getText();
+        String pass = password.getText();
+        String rol = role.getText();
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(rol);
+        ArrayList<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
+        grantedAuthorityList.add(grantedAuthority);
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                = new UsernamePasswordAuthenticationToken(userName, pass, grantedAuthorityList);
+
+        AuthenticationManager authenticationManager = applicationContext.getBean(AuthenticationManager.class);
+        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        if (authentication.isAuthenticated()) {
+            securityContext = new SecurityContextImpl();
+            securityContext.setAuthentication(authentication);
+            SecurityContextHolder.setContext(securityContext);
+            try {
+                myService.doSomething();
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("Successfully Executed doSomething.");
+                alert.showAndWait();
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText("ERROR 401.7");
+                alert.setContentText("ACCESS DENIED!");
+                alert.showAndWait();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("ERROR 401");
+            alert.setContentText("NOT AUTHENTICATED!");
+            alert.showAndWait();
+        }
+
+
+    }
+}
--- src/main/java/com/springsecurity/labs/labone/controller/MainController.java
+++ src/main/java/com/springsecurity/labs/labone/controller/MainController.java
@@ -1,8 +1,25 @@
 package com.springsecurity.labs.labone.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     public void openNewWindow(ActionEvent actionEvent) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/child.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setTitle("Spring Security Child");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
